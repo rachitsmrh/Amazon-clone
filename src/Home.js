b5a59326ad7fbacbb508abe6c33a204b23904af1
Rachit Singhal <rachitsinghal40@gmail.com>
2020-10-05 16:22:10 +0530

    navbar

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <p>Maor</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhS72N3HitwLwtNiLdDqTmdA-Qusko-Xg",
+  authDomain: "clone-8c3b4.firebaseapp.com",
+  databaseURL: "https://clone-8c3b4.firebaseio.com",
+  projectId: "clone-8c3b4",
+  storageBucket: "clone-8c3b4.appspot.com",
+  messagingSenderId: "574964764293",
+  appId: "1:574964764293:web:ee36b0fc2b95bd94cfa3ec",
+  measurementId: "G-NW54WE99VN",
+};
